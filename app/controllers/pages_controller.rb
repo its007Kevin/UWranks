a104000ad1d6f1ebca35c267711e419f74461128
kevin <kevinzeng98@gmail.com>
2018-01-06 18:07:27 -0500

    Added username, note: Profile link on username dropdown won't work because of routing currently

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,24 @@
 class PagesController < ApplicationController
+	# back-end code for pages/index
   def index
   end
 
+	# back-end code for pages/home
   def home
   end
 
+	# back-end code for pages/profile
   def profile
+  	# grab the username from the URL as :id\
+  	if (User.find_by_username(params[:id])) 
+  		@username = params[:id]
+  	else
+  		# redirect to error
+  		redirect_to root_path, :notice => "User not found!"
+  	end
   end
 
+  # back-end code for pages/explore
   def explore
   end
 end
