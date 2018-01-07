99eac00024a89045bb234a7ad8817a66f314b27e
kevin <kevinzeng98@gmail.com>
2018-01-07 11:14:17 -0500

    Added simple form to create posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
+		respond_to do |f|
+			if (@post.save)
+				f.html { redirect_to "", notice: "Post Created" }
+			else
+				f.html { redirect_to "", notice: "Error: Post Not Saved" }
+			end
+		end
+	end
+
+	private
+	def post_params # allows certain data to be passed via form
+		params.require(:post).permit(:user_id, :content)
+	end
+end
