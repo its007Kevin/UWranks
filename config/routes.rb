44d77e087db7936c156b3c9348b549bc98856e7a
kevin <kevinzeng98@gmail.com>
2018-01-10 18:40:46 -0500

    follower relationships, ui not refined yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :relationships
+  resources :users do
+  	member do
+  		get :following, :followers
+  	end
+  end
   resources :posts
   root 'pages#index'
 
