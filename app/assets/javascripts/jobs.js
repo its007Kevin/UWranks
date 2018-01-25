06e23fbf8dd6142d1ce90b6ac9b0d97222ebd636
kevin <kevinzeng98@gmail.com>
2018-01-24 23:15:07 -0500

    Create .gitignore file

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,11 +1,11 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(".selectJobBtn").click(function() {
+$(document).ready(function() {
+  $(".selectBtn").click(function() {
     var $row = $(this).closest("tr");  
-    var $text = $row.find(".jobTableId").text(); 
-    
+    var $text = $row.find(".jobId").text(); 
     alert($text);
+  });
 });
-
