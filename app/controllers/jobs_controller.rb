bdb740ae3258705fdb24b1e0739cdfed38312633
kevin <kevinzeng98@gmail.com>
2018-01-24 20:57:37 -0500

    Add javascript for selecting search result

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,11 @@
 class JobsController < ApplicationController
   def index 
     @search = Job.ransack(params[:q])
+    if params[:q].nil?
+      @emptyQuery = true
+    else
+      @emptyQuery = false
+    end
     @results = @search.result
   end
 
@@ -10,8 +15,11 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    @job.save
-    redirect_to jobs_path
+    if @job.save
+      redirect_to jobs_path
+    else
+      redirect_to "", :notice => "Job was not saved"
+    end
   end
 
   private
