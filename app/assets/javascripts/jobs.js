8f7f57d2abb9cf828983d848cc631eb33e3f5719
kevin <kevinzeng98@gmail.com>
2018-02-01 22:44:30 -0500

    Show / Hide Comments

--- app/assets/javascripts/jobs.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function() {
-//   $(".selectBtn").click(function() {
-//     var $row = $(this).closest("tr");  
-//     var $text = $row.find(".jobId").text(); 
-//     alert($text);
-//   });
-// });
