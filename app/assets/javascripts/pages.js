1e0a07d1d77cb5c7e98763b1cd11d3717d9320e7
kevin <kevinzeng98@gmail.com>
2018-02-01 22:44:30 -0500

    Show / Hide Comments

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('#commentsToggle').click(function () {
+    if ($(this).text() === "Show Comments") {
+      $(this).text("Hide Comments");
+    } else {
+      $(this).text("Show Comments");
+    }
+    $('.comments').toggle();
+ });
+});
