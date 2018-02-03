c7e5fd9a9920bb65e61fa80a9ff1b5f073983d2a
kevin <kevinzeng98@gmail.com>
2018-02-03 17:35:12 -0500

    Add ability to search for jobs and get back associated users and their rankings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
 
-  get 'post/login'
+  #get 'post/login'
 
   resources :relationships
   resources :users do
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   resources :conversations do
     resources :messages
   end
-  
+
   root 'pages#login'
 
   get '/home' => 'pages#home'
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   get '/explore' => 'pages#explore'
   get '/index' => 'pages#index'
   get '/login' => 'pages#login'
-  
+
   # For custom jobs index paths
   post '/jobs' => 'jobs#create'
   post '/rankings/new' => 'rankings#new'
