182cb0a388904eee2fa8c16202dda85b037ddb6f
kevin <kevinzeng98@gmail.com>
2018-02-05 23:03:17 -0500

    Style landing page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :user_id, presence: true # user id must be present when creating a post
   validates :content, presence: true
   default_scope -> { order(created_at: :desc) } # order by newest posts
