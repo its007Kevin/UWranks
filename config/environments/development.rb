928e1255f299b53358a7a29c3bf5d4db96598d6d
kevin <kevinzeng98@gmail.com>
2018-02-18 17:31:25 -0500

    Setup password reset email, fix change password bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,16 @@ Rails.application.configure do
   # For devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'uwranks@gmail.com',
+    password:             '23qz5cjE*',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
