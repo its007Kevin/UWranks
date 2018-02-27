25a9e6b1e9f43c6fd84ed9df40a6a4796f84d45d
kevinzeng98 <kevinzeng98@gmail.com>
2018-02-27 17:19:07 -0500

    Prepare model for validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,9 @@
 class Job < ApplicationRecord
   has_many :rankings
   has_many :posts
+  
+  validates :jobId, presence: true
+  validates :company, presence: true
+  validates :position, presence: true
+  validates :location, presence: true
 end
