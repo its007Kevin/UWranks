37e0d931212b0fe71b2e9a97e0ecbec1f8fdd7d0
Eric Wang <the.ericwang35@gmail.com>
2018-02-27 21:49:55 -0500

    add search bar to conversations

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -2,9 +2,15 @@ class InboxController < ApplicationController
   def index
     session[:conversations] ||= []
 
-    @users = User.all.where.not(id: current_user)
-    @conversations = Conversation.includes(:recipient, :messages)
-                                 .find(session[:conversations])
+    #@users = User.all.where.not(id: current_user)
+    @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
+
+
+    @users = if params[:user]
+    User.where('username LIKE ?', "%#{params[:user]}%")
+  else
+    User.all.where.not(id: current_user)
+  end
 
   end
 end
