4099d9575e77c1d72e829cdb1246c8003fa2275e
kevinzeng98 <kevinzeng98@gmail.com>
2018-03-02 15:16:42 -0500

    Add length validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
   validates :body, presence: true
+  validates_length_of :body, :maximum => 200
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -6,4 +6,8 @@ class Job < ApplicationRecord
   validates :company, presence: true
   validates :position, presence: true
   validates :location, presence: true
+  validates_length_of :jobId, :maximum => 10
+  validates_length_of :company, :maximum => 40
+  validates_length_of :position, :maximum => 100
+  validates_length_of :location, :maximum => 40
 end
