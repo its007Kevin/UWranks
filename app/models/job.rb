15968959a8e5287860b543154acf60943e6827f6
kevin <kevinzeng98@gmail.com>
2018-03-04 16:03:53 -0500

    Force job id to be 5 length, add new git ignore

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,7 +6,7 @@ class Job < ApplicationRecord
   validates :company, presence: true
   validates :position, presence: true
   validates :location, presence: true
-  validates_length_of :jobId, :maximum => 10
+  validates_length_of :jobId, :minimum => 5, :maximum => 5
   validates_length_of :company, :maximum => 40
   validates_length_of :position, :maximum => 100
   validates_length_of :location, :maximum => 40
