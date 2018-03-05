76c012398ca7e54ab19752f0a8c20583b7a6b0ed
kevin <kevinzeng98@gmail.com>
2018-03-04 22:17:44 -0500

    Remove mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,7 +20,6 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'uwranks@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
