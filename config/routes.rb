ff7f2df49d8ff25cea5340456b008eec683392dd
kevin <kevinzeng98@gmail.com>
2018-03-04 22:02:16 -0500

    Integrate cas authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, :controllers => {:sessions => "sessions"}
 
   resources :feedbacks
   resources :relationships
