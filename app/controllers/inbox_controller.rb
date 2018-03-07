55530c5949d3ac3dd1296daca8aff4d1a46a981a
Eric Wang <the.ericwang35@gmail.com>
2018-03-06 20:14:26 -0500

    only display active conversations in inbox

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -5,11 +5,16 @@ class InboxController < ApplicationController
     # @users = User.all.where.not(id: current_user)
     @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
 
+    @isSearch = false
 
-    @users = if params[:user]
-      User.where("username LIKE ?", "%#{params[:user]}%")
+
+    if params[:user]
+      @isSearch = true
+      @users = User.where("username LIKE ? AND username != ?", "%#{params[:user]}%", current_user.username)
     else
-      User.all.where.not(id: current_user)
+      @isSearch = false
+      @users = Conversation.where("recipient_id = ? OR sender_id = ?", current_user.id, current_user.id) #and the length of its messages is not 0
+      #@users = User.all.where.not(id: current_user)
     end
 
 
