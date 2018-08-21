201c4794487c7773ccd66aab10c6a8538b8d71f0
kevin <kevinzeng98@gmail.com>
2018-08-21 15:23:38 -0400

    Fix double add ranking bug

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -3,4 +3,5 @@ class Ranking < ApplicationRecord
   belongs_to :job
   validates :employerRank, presence: true
   validates :yourRank, presence: true
+  validates :job_id, uniqueness: { scope: :user_id }
 end
