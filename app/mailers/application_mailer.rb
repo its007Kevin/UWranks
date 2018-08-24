017474047c692a911adbc1083d56747899070b5d
Eric Wang <the.ericwang35@gmail.com>
2018-08-23 21:17:25 -0400

    finish up email for comments + change default email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-	default from: "uwranks123@gmail.com"
+	default from: "uwranks@gmail.com"
 
-	def sample_email(user)
+	def sample_email(user, postRanking)
     @user = user
-    #mail(to: @user.email, subject: 'Sample Email')
-    mail(to: 'ewang1242@gmail.com', subject: 'UWranks Email Test')
+    @postRanking = postRanking
+    @email = @user.username + "@edu.uwaterloo.ca"
+    mail(to: @email, subject: 'UWranks - Someone Commented on your Post!')
+    #mail(to: 's29zeng@edu.uwaterloo.ca', subject: 'UWranks - Someone Commented on your Post!')
+    #mail(to: 'ewang1242@gmail.com', subject: 'UWranks - Someone Commented on your Post!')
   end
 end
