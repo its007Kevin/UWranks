464ce2c99b96a696522744e854f59c8ab0b63a9f
Eric Wang <the.ericwang35@gmail.com>
2018-08-25 22:08:17 -0400

    switch from test email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,17 +5,17 @@ class ApplicationMailer < ActionMailer::Base
     @user = user
     @postRanking = postRanking
     @email = @user.username + "@edu.uwaterloo.ca"
-    #mail(to: @email, subject: 'UWranks - Someone Commented on your Post!')
+    mail(to: @email, subject: 'UWranks - Someone Commented on your Post!')
     #mail(to: 's29zeng@edu.uwaterloo.ca', subject: 'UWranks - Someone Commented on your Post!')
-    mail(to: 'ewang1242@gmail.com', subject: 'UWranks - Someone Commented on your Post!')
+    #mail(to: 'ewang1242@gmail.com', subject: 'UWranks - Someone Commented on your Post!')
   end
 
   def ranking_email(user, postRanking)
   	@user = user
   	@postRanking = postRanking
   	@email = @user.username + "@edu.uwaterloo.ca"
-  	#mail(to: @email, subject: 'UWranks - Another User Ranked a Job You\'re Interested In!!')
-  	mail(to: 'ewang1242@gmail.com', subject: 'UWranks - Another User Ranked a Job You\'re Interested In!')
+  	mail(to: @email, subject: 'UWranks - Another User Ranked a Job You\'re Interested In!!')
+  	#mail(to: 'ewang1242@gmail.com', subject: 'UWranks - Another User Ranked a Job You\'re Interested In!')
   end
 
 end
