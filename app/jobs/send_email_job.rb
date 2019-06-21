3f4fe5ff1491a5d61554ff4d878713176547067b
Kevin Zeng <kevinzeng98@gmail.com>
2019-06-20 20:17:09 -0700

    Disable email jobs for now

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -1,9 +1,9 @@
 class SendEmailJob < ActiveJob::Base
-  queue_as :default
+  # queue_as :default
 
   def perform(user, postRanking)
   	@user = user
     @postRanking = postRanking
-    ApplicationMailer.ranking_email(@user, @postRanking).deliver_later
+    # ApplicationMailer.ranking_email(@user, @postRanking).deliver_later
   end
 end
